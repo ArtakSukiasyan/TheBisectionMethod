bebe3af5ffc7bb1c65d395ddec7d1712dee4b3c6
ArtakSukiasyan <armen93g@gmail.com>
2021-09-17 21:44:27 +0400

    use While loop and find x

--- The_Bisection_Method/The_Bisection_Method.py
+++ The_Bisection_Method/The_Bisection_Method.py
@@ -4,4 +4,22 @@ def F(x):
 a=1;
 b=2;
 E=1e-2;
+x=(a+b)/2
+k=0
 
+while abs(F(x))>E:
+    if ((F(a)*F(x))<0):
+        b=x
+    else:
+        a=x
+  
+    k+=1
+    x=(a+b)/2
+    
+    
+
+print ("x = ", x)
+print ("step is equal ", k)
+
+
+    
